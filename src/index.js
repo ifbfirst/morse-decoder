e85f951f38db49e3af3d611a3551f11ba1da43f5
Kate Mashko <katika.life@gmail.com>
2023-10-20 10:46:37 +0300

    init:start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,9 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
-}
+   
+  let str = expr.split('');
+   let wordSym = '';
+   let wordCode = '';
+  
+  for (let i = str.length; i>0; i= i-10){
+    wordSym = '';
+   for (let a = i; a>i-10; a= a-2){
+    if( `${str[a-2]}${str[a-1]}` === '10') {wordSym = `.${wordSym}`}
+    else if (`${str[a-2]}${str[a-1]}` === '11' ) {wordSym = `-${wordSym}`}
+    else if (`${str[a-1]}${str[a-2]}${str[a-3]}${str[a-4]}${str[a-5]}${str[a-6]}${str[a-7]}${str[a-8]}${str[a-9]}${str[a-10]}` === '**********') {wordSym = ' '}
+    else {wordSym = `${wordSym}`}
+   }
+  
+   console.log(wordSym)
+   for (key in MORSE_TABLE) {
+        if(key === wordSym) {wordCode = `${MORSE_TABLE[key]}${wordCode}`;
+       } 
+      } 
+      if ( wordSym === ' ') {wordCode = ` ${wordCode}`}
+    }
+  
+   return wordCode;
+  }
 
 module.exports = {
     decode
